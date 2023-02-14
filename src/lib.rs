ea3443b9abddff41b9a051b3d719f5f241e4f626
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-02-14 14:36:42 -0500

    Basic socketcand connection

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,57 @@
+use anyhow::Result;
+use pyo3::{
+    prelude,
+    types::{IntoPyDict, PyDict, PyModule},
+    Py, Python, IntoPy,
+};
 
+pub struct PyCanInterface;
+
+impl PyCanInterface {
+    pub fn new_socketcan(dev: &str) {
+        Python::with_gil(|py| -> Result<()> {
+            let can = py.import("can")?;
+
+            let mut args = PyDict::new(py);
+            args.update(
+                [
+                    ("bustype", "socketcand"),
+                    ("host", "side"),
+                    ("channel", "vcan0"),
+                ].into_py_dict(py).as_mapping()
+            )?;
+
+            args.update(
+                [
+                    ("port", 30000)
+                ].into_py_dict(py).as_mapping()
+            )?;
+
+            let iface = can.getattr("interface")?.call_method(
+                "Bus",
+                (),
+                Some(args)
+            )?;
+
+            print!("{iface:?}");
+
+            loop {
+                let message = iface.call_method0("recv")?;
+
+                println!("{message:?}");
+            }
+
+            todo!()
+        }).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        PyCanInterface::new_socketcan("blah");
+    }
+}
