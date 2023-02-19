63e50bfe061319180fe4a478a8d435edb80ae4d7
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-02-19 01:41:26 -0500

    Basic send

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use message::PyCanMessage;
 use pyo3::{
-    types::{IntoPyDict, PyDict},
-    Py, PyAny, Python, ToPyObject,
+    types::{IntoPyDict, PyDict, PyTuple},
+    Py, PyAny, Python, ToPyObject, IntoPy,
 };
 
 pub mod message;
@@ -18,16 +18,17 @@ pub enum PyCanBusType {
 pub struct PyCanInterface {
     pub bustype: PyCanBusType,
     iface: Py<PyAny>,
+    pycan: Py<PyAny>,
 }
 
 impl PyCanInterface {
     pub fn new(kind: PyCanBusType) -> Result<Self> {
-        let iface = match &kind {
+        let (iface, pycan) = match &kind {
             PyCanBusType::Socketcand {
                 host,
                 channel,
                 port,
-            } => Python::with_gil(|py| -> Result<Py<PyAny>> {
+            } => Python::with_gil(|py| -> Result<_> {
                 let can = py.import("can")?;
 
                 let args = PyDict::new(py);
@@ -47,13 +48,14 @@ impl PyCanInterface {
                     .getattr("interface")?
                     .call_method("Bus", (), Some(args))?;
 
-                Ok(iface.to_object(py))
+                Ok((iface.to_object(py), can.to_object(py)))
             }),
         }?;
 
         Ok(Self {
             bustype: kind,
             iface,
+            pycan
         })
     }
 
@@ -64,6 +66,20 @@ impl PyCanInterface {
                 .unwrap()
                 .extract(py)
                 .unwrap()
+        })
+    }
+
+    pub fn send(&self, id: u32, data: &[u8]) {
+        Python::with_gil(|py| {
+            let kwargs = [
+                ("arbitration_id", id.to_object(py)),
+                ("data", data.to_object(py)),
+                ("dlc", data.len().to_object(py))
+            ].into_py_dict(py);
+
+            let msg = self.pycan.call_method(py, "Message", (), Some(kwargs)).unwrap();
+
+            self.iface.call_method1(py, "send", PyTuple::new(py, [msg])).unwrap();
         })
     }
 }
@@ -84,6 +100,8 @@ mod tests {
         loop {
             let message = can.recv();
             println!("recv {message}");
+
+            can.send(message.arbitration_id, &message.data.unwrap());
         }
     }
 }
