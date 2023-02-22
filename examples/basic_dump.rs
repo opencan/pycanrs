2d404947c813358fb66b44fa468ff49350f6ca8e
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-02-21 19:04:32 -0500

    Mimic candump format

--- examples/basic_dump.rs
+++ examples/basic_dump.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use pycanrs::*;
+use pycanrs::{*, message::PyCanMessage};
 
 #[derive(Subcommand)]
 enum Bus {
@@ -26,26 +26,41 @@ struct Args {
 pub fn main() -> Result<()> {
     let args = Args::parse();
 
-    let can = PyCanInterface::new(match args.bus {
+    let can = PyCanInterface::new(match &args.bus {
         Bus::Slcan {
             serial_port,
             bitrate,
         } => PyCanBusType::Slcan {
-            bitrate,
-            serial_port,
+            bitrate: *bitrate,
+            serial_port: serial_port.clone(),
         },
         Bus::Socketcand {
             host,
             port,
             channel,
         } => PyCanBusType::Socketcand {
-            channel,
-            host,
-            port,
+            channel: channel.clone(),
+            host: host.clone(),
+            port: *port,
         },
     })?;
 
-    let cb = |msg: &_| println!("{msg}");
+    let iface_name = match args.bus {
+        Bus::Slcan { serial_port, .. } => serial_port,
+        Bus::Socketcand { host, port, channel } => format!("{channel}@{host}:{port}"),
+    };
+
+    let cb = move |msg: &PyCanMessage| {
+        let mut data = String::new();
+        for byte in msg.data.as_ref().unwrap() {
+            data += &format!("{:X} ", byte);
+        }
+        let data = data.trim();
+
+        println!("  {iface_name}  {:08X}   [{}]  {data}",
+                msg.arbitration_id, msg.dlc.unwrap());
+    };
+
     let err_cb = |err: &_| {
         eprintln!("{err}");
         std::process::exit(-1);
